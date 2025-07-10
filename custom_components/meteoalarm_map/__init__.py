6b1a31537549f1bffd58158ebb3e250e617da1c6
Rene Kroon <rene@connect-smart.nl>
2025-07-10 10:11:12 +0200

    forward setups

--- custom_components/meteoalarm_map/__init__.py
+++ custom_components/meteoalarm_map/__init__.py
@@ -1,18 +1,29 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 
-async def async_setup(hass, config):
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the Meteoalarm Map component."""
     return True
 
-async def async_setup_entry(hass, entry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up Meteoalarm Map from a config entry."""
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN]["config"] = entry.data
 
-    await hass.config_entries.async_forward_entry_setup(entry, "camera")
-    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    # Forward the setup to the camera and sensor platforms
+    await hass.config_entries.async_forward_entry_setups(entry, ["camera", "sensor"])
 
     return True
 
-async def async_unload_entry(hass, entry):
-    unload_camera = await hass.config_entries.async_forward_entry_unload(entry, "camera")
-    unload_sensor = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
-    return unload_camera and unload_sensor
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["camera", "sensor"])
+    
+    if unload_ok:
+        # Remove the config data
+        hass.data[DOMAIN].pop("config", None)
+        if not hass.data[DOMAIN]:
+            hass.data.pop(DOMAIN, None)
+    
+    return unload_ok
