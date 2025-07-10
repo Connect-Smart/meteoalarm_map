b7de376e02d01d479d44198ec43ad7035a7b37ae
Rene Kroon <rene@connect-smart.nl>
2025-07-10 09:59:21 +0200

    str to lost

--- custom_components/meteoalarm_map/config_flow.py
+++ custom_components/meteoalarm_map/config_flow.py
@@ -7,6 +7,8 @@ class MeteoalarmMapConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
+            # splits landen op komma's, verwijder spaties
+            user_input["countries"] = [c.strip().lower() for c in user_input["countries"].split(",")]
             return self.async_create_entry(title="Meteoalarm Map", data=user_input)
 
         return self.async_show_form(
@@ -15,6 +17,6 @@ class MeteoalarmMapConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 vol.Required("update_interval", default=10): int,
                 vol.Optional("vacation_start", default="2025-08-01"): str,
                 vol.Optional("vacation_end", default="2025-08-25"): str,
-                vol.Optional("countries", default=["italy", "spain", "france"]): [str]
+                vol.Optional("countries", default="italy, spain, france"): str
             })
         )
